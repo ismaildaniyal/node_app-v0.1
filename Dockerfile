722918b6c06e4be0429c7ef498e44d74aa7dcd66
ismail <ismail@kali>
2025-02-17 01:35:06 +0530

    db connected cp succeffully done

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,27 @@
-FROM node:18
+# Stage 1: Build
+FROM node:alpine AS builder
 
-WORKDIR /App
+WORKDIR /app
 
-COPY package.json ./
+# Copy package.json and package-lock.json first (for caching)
+COPY package.json package-lock.json ./
 
-RUN npm install
+# Install dependencies including mongoose
+RUN npm install --only=production
 
+# Stage 2: Run
+FROM node:alpine
+
+WORKDIR /app
+
+# Copy dependencies from the builder stage
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package.json ./package.json
+
+# Copy all application files
 COPY . .
 
 EXPOSE 3000
 
 CMD ["node", "index.js"]
-
-
-
 
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+const dbURI = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+mongoose.connect('mongodb://mongo_container:27017/mydatabase')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js App!');
+    res.send('MongoDB is connected successfully');
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
